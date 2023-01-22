781f78dceec174dbb8bde880c178d7ce37916f20
Nils <40796807+sonsan@users.noreply.github.com>
2023-01-22 16:32:41 +0100

    moved lsp keybinds

--- lua/nils/configuration/lsp.lua
+++ lua/nils/configuration/lsp.lua
@@ -1,4 +1,8 @@
 local conf = {}
+local Remap = require("nils.keymap")
+
+local nnoremap = Remap.nnoremap
+local inoremap = Remap.inoremap
 
 function conf.trouble()
   require("trouble").setup({
@@ -42,6 +46,25 @@ function conf.nullls()
 end
 
 function conf.lspzero()
+  local opts = { buffer = bufnr, remap = false, silent = true }
+  nnoremap("gd", function()
+    vim.lsp.buf.definition()
+  end, opts)
+  nnoremap("gi", function()
+    vim.lsp.buf.implementation()
+  end, opts)
+  nnoremap("<leader>vws", function()
+    vim.lsp.buf.workspace_symbol()
+  end, opts)
+  nnoremap("<leader>vd", "<cmd>Lspsaga show_cursor_diagnostics<CR>", opts)
+  inoremap("<C-h>", "<cmd>Lspsaga signature_help<CR>", opts)
+  nnoremap("]d", "<Cmd>Lspsaga diagnostic_jump_next<CR>", opts)
+  nnoremap("[d", "<Cmd>Lspsaga diagnostic_jump_prev<CR>", opts)
+  nnoremap("K", "<Cmd>Lspsaga hover_doc<CR>", opts)
+  nnoremap("<leader>gD", "<Cmd>Lspsaga lsp_finder<CR>", opts)
+  nnoremap("<leader>gr", "<Cmd>Lspsaga rename<CR>", opts)
+  nnoremap("<leader>vca", "<cmd>Lspsaga code_action<CR>", opts)
+
   local status1, lsp = pcall(require, "lsp-zero")
   if not status1 then
     print("lsp-zero not installed")
