0694253b0845dcb6a97df8eae0e8e11fc535d317
Nils <40796807+uchars@users.noreply.github.com>
2023-02-17 10:55:06 +0100

    lsp not lazy

--- lua/nils/plugins/lsp.lua
+++ lua/nils/plugins/lsp.lua
@@ -1,56 +1,40 @@
 local conf = require("nils.configuration.lsp")
 
-local lsp_filetype = {
-  "typescript",
-  "typescriptreact",
-  "javascriptreact",
-  "javascript",
-  "dart",
-  "html",
-  "css",
-  "c",
-  "sh",
-  "lua",
-  "markdown",
-  "dockerfile",
-  "yaml",
-}
-
 return {
-  {
-    "VonHeikemen/lsp-zero.nvim",
-    config = conf.lspzero,
-    ft = lsp_filetype,
-    dependencies = {
-      -- LSP Support
-      { "neovim/nvim-lspconfig" }, -- Required
-      {
-        "williamboman/mason.nvim",
-      }, -- Optional
-      {
-        "williamboman/mason-lspconfig.nvim",
-      }, -- Optional
+    {
+        "VonHeikemen/lsp-zero.nvim",
+        config = conf.lspzero,
+        lazy = false,
+        dependencies = {
+            -- LSP Support
+            { "neovim/nvim-lspconfig" }, -- Required
+            {
+                "williamboman/mason.nvim",
+            }, -- Optional
+            {
+                "williamboman/mason-lspconfig.nvim",
+            }, -- Optional
 
-      -- Autocompletion
-      { "hrsh7th/nvim-cmp" }, -- Required
-      { "hrsh7th/cmp-nvim-lsp" }, -- Required
-      { "hrsh7th/cmp-buffer" }, -- Optional
-      { "hrsh7th/cmp-path" }, -- Optional
-      { "saadparwaiz1/cmp_luasnip" }, -- Optional
-      { "hrsh7th/cmp-nvim-lua" }, -- Optional
+            -- Autocompletion
+            { "hrsh7th/nvim-cmp" }, -- Required
+            { "hrsh7th/cmp-nvim-lsp" }, -- Required
+            { "hrsh7th/cmp-buffer" }, -- Optional
+            { "hrsh7th/cmp-path" }, -- Optional
+            { "saadparwaiz1/cmp_luasnip" }, -- Optional
+            { "hrsh7th/cmp-nvim-lua" }, -- Optional
 
-      -- Snippets
-      { "L3MON4D3/LuaSnip" }, -- Required
+            -- Snippets
+            { "L3MON4D3/LuaSnip" }, -- Required
 
-      -- Visual stuff
-      {
-        "jose-elias-alvarez/null-ls.nvim",
-        config = conf.nullls,
-      },
-      {
-        "folke/trouble.nvim",
-        config = conf.trouble,
-      },
+            -- Visual stuff
+            {
+                "jose-elias-alvarez/null-ls.nvim",
+                config = conf.nullls,
+            },
+            {
+                "folke/trouble.nvim",
+                config = conf.trouble,
+            },
+        },
     },
-  },
 }
