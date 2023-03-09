b65adf4d265ccfffdba214296ae09c565abfb2de
Nils <40796807+uchars@users.noreply.github.com>
2023-03-09 16:53:41 +0100

    fix lsp error jump & added gitblame stuff to cmd menu

--- lua/nils/configuration/git.lua
+++ lua/nils/configuration/git.lua
@@ -2,6 +2,8 @@ local conf = {}
 
 function conf.git()
   vim.g.gitblame_display_virtual_text = 0
+  vim.g.gitblame_date_format = "%r"
+  vim.g.gitblame_message_template = "<author>, <date> • <summary>"
   require("gitsigns").setup({})
 end
 
