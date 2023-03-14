09a0ef15255e52242daef89e4d67095f8747eace
Noah4ever <noah.thiering@web.de>
2023-03-14 14:49:22 +0100

    Remove ChatGPT because of payment

--- lua/nils/plugins/chatgpt.lua
+++
@@ -1,9 +0,0 @@
-local conf = require("nils.configuration.chatgpt")
-return {
-  "jackMort/ChatGPT.nvim",
-  lazy = false,
-  dependencies = {
-    "MunifTanjim/nui.nvim",
-  },
-  config = conf.chatgpt,
-}
