f468f81a0229f00a7b859afe35a1057aadd8eda6
Noah4ever <noah.thiering@web.de>
2023-03-21 16:42:09 +0100

    Add config.json to save preferences

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,14 +1,27 @@
 local Path = require("plenary.path")
-local colorscheme_path = vim.fn.stdpath("data") .. "/colorscheme.txt"
-vim.g.nils_colorscheme = "kanagawa"
--- Load colorscheme from preferences
-if Path:new(colorscheme_path):exists() then
-  vim.g.nils_colorscheme = Path:new(colorscheme_path):read()
+
+local config = {
+  colorscheme = "kanagawa",
+  transparency = false,
+}
+
+local config_path = vim.fn.stdpath("data") .. "/config.json"
+if Path:new(config_path):exists() then
+  local config_json = vim.fn.json_decode(Path:new(config_path):read()) -- read config json
+  -- merge config tables
+  for k, v in pairs(config_json) do
+    config[k] = v
+  end
+else
+  Path:new(config_path):write(vim.fn.json_encode(config), "w") -- write config if file not exists
 end
 
+vim.cmd.colorscheme(config.colorscheme) -- set colorscheme
+if config.transparency == true then
+  vim.cmd("TransparentEnable")          -- set transparency
+end
 vim.opt.termguicolors = true
 vim.opt.background = "dark"
-vim.cmd.colorscheme(vim.g.nils_colorscheme)
 
 local hl = function(thing, opts)
   vim.api.nvim_set_hl(0, thing, opts)
