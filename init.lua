833777ad46aa13c0fb29b87a665fb6796a03a987
uchars <40796807+uchars@users.noreply.github.com>
2023-04-07 15:51:51 +0200

    removed nvim-notify & chat-gpt

--- init.lua
+++ init.lua
@@ -51,5 +51,3 @@ require("lazy").setup("nils.plugins", {
     },
   },
 })
-
-vim.notify = require("notify")
--- lua/nils/plugins/notifications.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "rcarriga/nvim-notify",
-  event = "UIEnter",
-  config = {
-    background_colour = "#000000",
-    top_down = false,
-  },
-}
