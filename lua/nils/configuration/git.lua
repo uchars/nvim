217dbae0d8dca5e80955ec4415110b6dc7aad4a2
uchars <40796807+uchars@users.noreply.github.com>
2023-04-09 10:15:08 +0200

    feat: keybinds for git merge

--- lua/nils/configuration/git.lua
+++ lua/nils/configuration/git.lua
@@ -1,10 +1,15 @@
 local conf = {}
+local Remap = require("nils.keymap")
+local nnoremap = Remap.nnoremap
 
 function conf.git()
   vim.g.gitblame_display_virtual_text = 0
   vim.g.gitblame_date_format = "%r"
   vim.g.gitblame_message_template = "<author>, <date> • <summary>"
   require("gitsigns").setup({})
+  nnoremap("<leader>gs", "<cmd>Git<CR>")
+  nnoremap("gf", "<cmd>diffget //2<CR>")
+  nnoremap("gj", "<cmd>diffget //3<CR>")
 end
 
 return conf
