abf541cd2651482a25631edbbeaa5c43ff180e6d
Noah4ever <noah.thiering@web.de>
2023-04-21 16:44:08 +0200

    remove singleton

--- lua/utils/configger.lua
+++ lua/utils/configger.lua
@@ -8,38 +8,37 @@ Configger = {
 -- Creates a new Configger object
 -- @param config The default config
 function Configger:new(config)
-  if Configger._instance then
-    return Configger._instance
-  end
-
   local configger = {}
   setmetatable(configger, self)
   self.__index = self
+
   configger.config_path = vim.fn.stdpath("data") .. "/configger.json"
   configger.config_file = Path:new(configger.config_path)
 
   if configger.config_file:exists() then
-    local config_json = vim.fn.json_decode(configger.config_file:read())      -- Read the config file
+    local config_json = vim.fn.json_decode(configger.config_file:read()) -- Read the config file
     configger.config = vim.tbl_extend("force", configger.config, config_json) -- Merge the config with the default config
   else
-    configger.config_file:write(vim.fn.json_encode(configger.config), "w")    -- Write the default config to the config file if it doesn't exist
+    configger.config_file:write(vim.fn.json_encode(configger.config), "w") -- Write the default config to the config file if it doesn't exist
     configger.config = config
   end
-  Configger._instance = configger
 
   return configger
+end
+
+function Configger:readConfig()
+  if self.config_file:exists() then
+    local config_json = vim.fn.json_decode(self.config_file:read()) -- Read the config file
+    self.config = vim.tbl_extend("force", self.config, config_json) -- Merge the config with the default config
+  else
+    self.config_file:write(vim.fn.json_encode(self.config), "w") -- Write the default config to the config file if it doesn't exist
+  end
+  return self.config
 end
 
 -- Returns the full config
 -- @return The full config
 function Configger:getConfig()
-  if self.config_file:exists() then
-    local config_json = vim.fn.json_decode(self.config_file:read()) -- Read the config file
-    self.config = vim.tbl_extend("force", self.config, config_json) -- Merge the config with the default config
-  else
-    self.config_file:write(vim.fn.json_encode(self.config), "w")    -- Write the default config to the config file if it doesn't exist
-  end
-
   return self.config
 end
 
@@ -68,13 +67,6 @@ end
 -- @return The path of the config file
 function Configger:getPath()
   return self.config_path
-end
-
--- Sets the path of the config file
--- @param path The path of the config file
-function Configger:setPath(path)
-  self.config_path = path
-  self.config_file = Path:new(self.config_path)
 end
 
 return Configger
