c9eb77125d391507d624d933424365d89e83493c
uchars <40796807+uchars@users.noreply.github.com>
2023-04-25 22:29:53 +0200

    lazy loads them on ':colorscheme x'

--- lua/nils/plugins/color-stuff.lua
+++ lua/nils/plugins/color-stuff.lua
@@ -2,17 +2,17 @@ local statuslineConf = require("nils.configuration.statusline")
 local colorConf = require("nils.configuration.color-stuff")
 
 return {
-  { "ellisonleao/gruvbox.nvim",    lazy = false, config = colorConf.gruvbox },
-  { "xiyaowong/nvim-transparent",  lazy = false, config = colorConf.transparent },
-  { "projekt0n/github-nvim-theme", lazy = false, config = colorConf.github },
-  { "rebelot/kanagawa.nvim",       lazy = false, config = colorConf.kanagawa },
-  { "Everblush/everblush.nvim",    lazy = false, config = colorConf.everblush },
-  { "folke/tokyonight.nvim",       lazy = false, config = colorConf.tokyonight },
-  { "sam4llis/nvim-tundra",        lazy = false, config = colorConf.tundra },
-  { "EdenEast/nightfox.nvim",      lazy = false, config = colorConf.nightfox },
-  { "Mofiqul/vscode.nvim",         lazy = false, config = colorConf.vscode },
-  { "catppuccin/nvim",             lazy = false, config = colorConf.catppuccin },
-  { "olimorris/onedarkpro.nvim",   lazy = false, config = colorConf.onedarkpro },
+  { "ellisonleao/gruvbox.nvim",    lazy = true, config = colorConf.gruvbox },
+  { "xiyaowong/nvim-transparent",  lazy = true, config = colorConf.transparent },
+  { "projekt0n/github-nvim-theme", lazy = true, config = colorConf.github },
+  { "rebelot/kanagawa.nvim",       lazy = true, config = colorConf.kanagawa },
+  { "Everblush/everblush.nvim",    lazy = true, config = colorConf.everblush },
+  { "folke/tokyonight.nvim",       lazy = true, config = colorConf.tokyonight },
+  { "sam4llis/nvim-tundra",        lazy = true, config = colorConf.tundra },
+  { "EdenEast/nightfox.nvim",      lazy = true, config = colorConf.nightfox },
+  { "Mofiqul/vscode.nvim",         lazy = true, config = colorConf.vscode },
+  { "catppuccin/nvim",             lazy = true, config = colorConf.catppuccin },
+  { "olimorris/onedarkpro.nvim",   lazy = true, config = colorConf.onedarkpro },
   {
     "folke/zen-mode.nvim",
     event = "VeryLazy",
