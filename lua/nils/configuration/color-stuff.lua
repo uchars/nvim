badda2e617dc5f06dd0421188dff1bd88d68c761
Nils <40796807+uchars@users.noreply.github.com>
2023-05-17 13:19:37 +0200

    transparency stuff

--- lua/nils/configuration/color-stuff.lua
+++ lua/nils/configuration/color-stuff.lua
@@ -35,6 +35,8 @@ function conf.transparent()
   require("transparent").setup({
     extra_groups = {
       "TelescopeNormal",
+      "TreesitterContext",
+      "FloatBorder",
       "NormalFloat",
       "NvimTreeNormal",
       "TelescopePrompt",
