fb98a2c1d5ab6d8cdc23012b0c428033e3842827
Nils <40796807+uchars@users.noreply.github.com>
2023-05-22 17:48:31 +0200

    fix: g(oto) f(ile) not working due to git diffget

--- lua/nils/configuration/git.lua
+++ lua/nils/configuration/git.lua
@@ -8,8 +8,8 @@ function conf.git()
   vim.g.gitblame_message_template = "<author>, <date> • <summary>"
   require("gitsigns").setup({})
   nnoremap("<leader>gs", "<cmd>Git<CR>")
-  nnoremap("gf", "<cmd>diffget //2<CR>")
-  nnoremap("gj", "<cmd>diffget //3<CR>")
+  nnoremap("gF", "<cmd>diffget //2<CR>")
+  nnoremap("gJ", "<cmd>diffget //3<CR>")
 end
 
 return conf
