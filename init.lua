0986d8b09508b50913dc3bb4fca04834733d2f82
Nils <40796807+uchars@users.noreply.github.com>
2023-11-08 20:09:54 +0100

    rename

--- init.lua
+++ init.lua
@@ -475,19 +475,11 @@ local on_attach = function(_, bufnr)
   end, { desc = 'Format current buffer with LSP' })
 end
 
--- Enable the following language servers
---  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
---
---  Add any additional override configuration in the following tables. They will be passed to
---  the `settings` field of the server config. You must look up that documentation yourself.
---
---  If you want to override the default filetypes that your language server will attach to you can
---  define the property 'filetypes' to the map in question.
 local servers = {
   clangd = {},
   pyright = {},
   rust_analyzer = {},
-  go = {},
+  gopls = {},
   nil_ls = {},
   tsserver = {},
   cmake = {},
